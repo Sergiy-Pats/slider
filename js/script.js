6252daeed7c70bf1cb08809173d867e2eda3e4ab
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-17 00:42:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+jQuery('document').ready(function () {
+	jQuery('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 700,
+		//easing: 'ease',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: false,
+		autoplaySpeed: 1800,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false,
+		swipe: true,
+		touchTreshold: 1,
+		touchMove: false,
+		waitForAnimate: true,
+		centerMode: true,
+		variableWidth: false,
+		rows: 1,
+		slidesPerRow: 1,
+		vertical: false,
+		verticalSwiping: false,
+		asNavFor: '.sliderbig',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					centerMode: false,
+				}
+			}
+		],
+		mobileFirst: false,
+		//appendArrows: '.slide'
+		//appendDots:'',
+	});
+	/*jQuery('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+		alert('NextSlide');
+	})*/
+	/*jQuery('.slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+
+		var sld = 'currentSlide';
+	})*/
+
+
+
+
+
+	jQuery('.sliderbig').slick({
+		arrows: false,
+		slidesToShow: 1,
+		fade: true,
+		asNavFor: '.slider'
+	})
+
+
+	jQuery('.slider').slick('setPosition');
+	jQuery('.slider').slick('goTo', 2)
+
+	jQuery('.link_pause').click(function (event) {
+		jQuery('.slider').slick('slickPause')
+	})
+	jQuery('.link_play').click(function (event) {
+		jQuery('.slider').slick('slickPlay')
+	});
+
+	$('.link__add').click(function (event) {
+		$('.slider').slick('slickAdd', '<div class="slider__item"><img src="https://klike.net/uploads/posts/2019-05/1558767816_12.jpg" alt="ОЙ КАРТИКИ НЕТ"></div>');
+		$('.sliderbig').slick('slickAdd', '<div class="slider__item"><img src="https://klike.net/uploads/posts/2019-05/1558767816_12.jpg" alt="ОЙ КАРТИКИ НЕТ"></div>');
+		return false;
+
+	})
+
+
+	$('.link__remove').click(function (event,) {
+		$('.slider').slick('slickRemove', 0);
+		$('.sliderbig').slick('slickRemove', 0);
+		return false;
+	})
+
+	//$('.slider').slick("unslick");
+
+});
+
